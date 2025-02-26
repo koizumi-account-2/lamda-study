2d0657283c79d88bde9074c65e6d5deb9b2a7ee5
yositaka.koizumi <yositaka.koizumi@esco.co.jp>
2025-02-26 19:50:59 +0900

    resize-sandbox-1

--- lambdas/resize/sandbox/resize.ts
+++ lambdas/resize/sandbox/resize.ts
@@ -1,10 +1,21 @@
-
+import jimp from "jimp"
 import path from "path"
 
-const REPOSITORY_TOP = path.resolve(__dirname,"../../../");
+const REPOSITORY_TOP = path.resolve(__dirname,"../../");
 
 async function main(){
-    const imagePath = path.join(REPOSITORY_TOP,"images/fuji.png");
+    const imagePath = path.join(REPOSITORY_TOP,"resize/images/fuji.png");
     console.log(`reading an image form ${imagePath}`);
+    const image = await jimp.read(imagePath);
+    const width = image.getWidth();
+    const height = image.getHeight();
+
+    console.log(`変更前のsize: ${width} ,${height}`);
+    const resizedWidth = Math.floor(width/2);
+    const resizedHeight = Math.floor(height/2);
+    console.log(`変更後のsize: ${resizedWidth} ,${resizedHeight}`);
+
+    image.resize(resizedWidth,resizedHeight);
+    image.write('images/resized_fuji.png');
 }
 main();
